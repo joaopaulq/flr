70c4ff86c3ec28c8cd1a3a5d35a635ce3615a8b4
João Paulo <jpgf@cin.ufpe.br>
2021-05-01 10:40:52 -0300

    função pra dividir o conjunto de dados, implementação da normalização

--- Preprocessing.hs
+++ Preprocessing.hs
@@ -2,11 +2,30 @@ module Preprocessing where
 
 import Util
   ( mean
-  , var
   , stdev
+  , transpose
   )
 
+-- | Normalize the dataset by subtract the mean and
+-- divide by the standard deviation of each feature.
+normalize :: [[Float]] -- ^ A dataset.
+          -> [[Float]] -- ^ Normalized dataset.
+normalize xs = [zipWith (/) (zipWith (-) x us) os | x <- xs]
+  where
+    us = map (mean) $ transpose xs
+    os = map (stdev) $ transpose xs
 
--- Normaliza cada [i, j]: z = ([i, j] - mean([:, j])) / stdev([:, j]).
-normalize :: [[Float]] -> [[Float]]
-normalize xs =
+
+-- | Split the dataset and the targets values
+-- into training and test sets.
+splitTrainTest :: [[Float]]            -- ^ A dataset.
+               -> [Float]              -- ^ Targets values.
+               -> Int                  -- ^ Number of examples in training set.
+               -> ([[Float]], [Float],
+                   [[Float]], [Float]) -- ^ (training set, training targets, test set, test targets)
+splitTrainTest xs y n = (xtrain, ytrain, xtest, ytrain)
+  where
+    xtrain = take n xs
+    ytrain = take n y
+    xtest  = drop n xs
+    ytest  = drop n y
