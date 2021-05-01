c6d4d60079ea70b5c61db328ce0c701df8dc4f50
João Paulo <jpgf@cin.ufpe.br>
2021-05-01 10:40:27 -0300

    implementação de loss e predict, e parcial de train

--- Model.hs
+++ Model.hs
@@ -1,16 +1,31 @@
 module Model where
 
-import Util (dot)
+import Util (dot, transpose)
 
+-- Computes the linear regression equation.
+predict :: ([Float], Float) -> [Float] -> Float
+predict (w, b) x = dot w x + b
 
--- Regressão linear.
-predict :: [Float] -> [Float] -> Float
-predict w x = dot w x
-
--- Função de perda.
+-- Computes the mean square error (L2 loss).
 loss :: [Float] -> [Float] -> Float
-loss y yHat = -- TODO
+loss yhat y = (1/(2*m)) * sum $ map (^2) $ zipWith (-) yhat y
+  where
+    m = fromIntegral $ length y
 
--- Gradiente descendente.
-train :: [Float] -> [[Float]] -> [Float]
-train w xs = -- TODO
+-- Performs the gradient descent algorithm.
+train :: ([Float], Float) -- ^ Coefficients.
+      -> [[Float]]        -- ^ A dataset.
+      -> [Float]          -- ^ Targets values.
+      -> Float            -- ^ Learning rate.
+      -> Int              -- ^ Max number of iterations.
+      -> ([Float], Float) -- ^ Updated coefficients.
+train (w, b) xs y lr maxiter
+  | maxiter == 0 = (w, b)
+  | otherwise do
+      print $ loss hx y
+      let w = w - -- TODO
+      let b = b - (lr * (1/m) * sum $ zipWith (-) hx y)
+      train (w, b) xs y lr (maxiter-1)
+        where
+          m  = fromIntegral $ length w
+          hx = map (predict w) xs
