d69d11adf69867d7ff81c261bef432b616b924ad
João Paulo <jpgf@cin.ufpe.br>
2021-05-01 10:41:18 -0300

    só um esboço do main

--- Main.hs
+++ Main.hs
@@ -1,14 +1,18 @@
 module Main where
 
-import Model
+import Model (train)
 import Preprocessing
+import Util (transpose)
 
 
 main :: IO ()
 main = do
-  -- Lê um arquivo csv de /data.
-  -- Converte pra Floats (?).
-  -- Preprocessa os dados.
-  -- Resolve a bronca do bias.
-  -- Treina.
+  -- Lê um arquivo csv de /data e converte pra [[Float]] (?).
+  let dataset = -- TODO
+  let (w, b) = -- ([random], random)
+  let (xtrain, ytrain, xtest, ytest)
+    = splitTrainTest $ normalize dataset
+  let lr = 0.01
+  let maxiter = 100
+  let (w', b') = train (w, b) xtrain ytrain lr maxiter
   -- Plota e salva em /logs.
