1dfa9029af0dc849865584a4c44e25f218405343
Thiago <tcnl@cin.ufpe.br>
2021-05-01 23:42:05 -0300

    tentando ler o dataset/csv

--- Main.hs
+++ Main.hs
@@ -1,5 +1,12 @@
 module Main where
 
+import Text.ParserCombinators.Parsec
+import Data.String.CSV
+
+import qualified Data.ByteString.Lazy as BL
+import Data.Csv
+import qualified Data.Vector as V
+
 import Model (train)
 import Preprocessing
 import Util (transpose)
@@ -8,7 +15,15 @@ import Util (transpose)
 main :: IO ()
 main = do
   -- Lê um arquivo csv de /data e converte pra [[Float]] (?).
-  let dataset = -- TODO
+  -- let dataset <- parseFromFile csvFile "./data/forestfires.csv"
+
+  csvData <- BL.readFile "./data/forestfires.csv"
+    case decode NoHeader csvData of
+        Left err -> putStrLn err
+        --Right v -> V.forM_ v ( printMyStuff ) --this works
+        Right v -> do let mylist = V.toList v :: [[Float]]
+        print v --Ta dando erro aqui, mas a ideia eh ler o .csv e colocar ele numa lista de floats
+        
   let (w, b) = -- ([random], random)
   let (xtrain, ytrain, xtest, ytest)
     = splitTrainTest $ normalize dataset
