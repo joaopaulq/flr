c8ecb8e24f0114bf159c17aa08ffeb0ad202c429
João Paulo <jpgf@cin.ufpe.br>
2021-05-03 13:43:48 -0300

    main com prints

--- Main.hs
+++ Main.hs
@@ -2,19 +2,27 @@ module Main where
 
 import Preprocessing
 import Model
-  ( fit
-  , predict
-  )
+import Util
 
 
 main :: IO ()
 main = do
   let dataset = [[1, 1, 3, 3], [1, 2, 2, 8], [2, 2, 1, 11],
                  [2, 3, 3, 13], [3, 5, 18, 8], [3, 2, -1, 15]]
-  let (w, b) = ([1, 4, 2], -1) -- True values: w = (2, 4, 1), b = -1.
+  let (w, b) = ([13, -8, 3], 5) -- True values: w = (2, 4, -1), b = -1.
   let (xtrain, ytrain, xtest, ytest) = splitTrainTest dataset 4
-  let lr = 0.01
-  let maxiter = 10000
-  let (w', b') = fit (w, b) xtrain ytrain lr maxiter
-  print (w', b')
+  -- let (xtrainNorm, xtestNorm) = (normalize xtrain, normalize xtest)
+
+  let (w', b') = fit (w, b) xtrain ytrain 0.01 10000
+  let yhat = map (predict (w', b')) xtrain
+
+  putStr "Loss on training: "
+  print $ loss ytrain yhat
+  putStr "Coefficients: "
+  print w'
+  putStr "Intercept: "
+  print b'
+  putStr "Test predict: "
   print $ map (predict (w', b')) xtest
+  putStr "Test expected: "
+  print ytest
